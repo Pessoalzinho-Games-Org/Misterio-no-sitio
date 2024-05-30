b97850900cbbf2e18acb294b657c7075ac4a9546
Henrique Mazzoleni <73548364+Henrique-mazzoleni@users.noreply.github.com>
2024-05-30 23:17:03 +0200

    background and player image added

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ SCREEN_HEIGHT = 600
 # Create the screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# Create the player
-player = pygame.Rect((300, 250, 50, 50))
+# Set the background image and scale it to the screen size
+background = pygame.image.load('test-bg.webp')
+background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+# Load the player image, scale it down to 10px and place it in the center of the screen
+player_image = pygame.image.load('player.png')
+player_image = pygame.transform.scale(player_image, (30, 30))
+player_rect = player_image.get_rect()
+player_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
 
 while True:
     # Get the state of the keyboard
@@ -17,13 +26,13 @@ while True:
     
     # Move the player based on the pressed keys
     if key[pygame.K_LEFT]:
-        player.x -= 1
+        player_rect.x -= 1
     if key[pygame.K_RIGHT]:
-        player.x += 1
+        player_rect.x += 1
     if key[pygame.K_UP]:
-        player.y -= 1
+        player_rect.y -= 1
     if key[pygame.K_DOWN]:
-        player.y += 1
+        player_rect.y += 1
 
     # Check for Exit event and quit the game
     for event in pygame.event.get():
@@ -34,8 +43,11 @@ while True:
     # Clear the screen
     screen.fill((0, 0, 0))
 
+    # Draw the background
+    screen.blit(background, (0, 0))
+
     # Draw the player
-    pygame.draw.rect(screen, (255, 255, 255), player)
+    screen.blit(player_image, player_rect)
 
     # Update the screen
     pygame.display.flip()
